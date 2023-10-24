b9fe5acfeae084ac95341514d9612d6fc5741a58
utkarsh032 <utkarshhraj525@gmail.com>
2023-10-24 09:01:19 +0530

    creating a collection of documents

--- mern-server/index.js
+++ mern-server/index.js
@@ -31,6 +31,9 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    // create a collection of documents
+    const bookCollections = client.db("BookInventory").collection("books");
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
