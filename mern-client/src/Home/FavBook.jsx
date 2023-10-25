6ef862d42741b3fe948b3e84d8ed0e3b62280f68
utkarsh032 <utkarshhraj525@gmail.com>
2023-10-25 22:54:53 +0530

    completed FavBook, PromoBanner

--- mern-client/src/Home/FavBook.jsx
+++ mern-client/src/Home/FavBook.jsx
@@ -1,10 +1,51 @@
+import { Link } from "react-router-dom";
+import FavBookImg from "../assets/favoritebook.jpg";
 
 const FavBook = () => {
   return (
-    <div>
-      FavBook
-    </div>
-  )
-}
+    <div className="px-4 lg:px-24 my-20 flex flex-col md:flex-row justify-between items-center gap-12">
+      <div className="md:w-1/2">
+        <img src={FavBookImg} alt="" className="rounded md:w-10/12" />
+      </div>
 
-export default FavBook
+      <div className="md:w-1/2 space-y-6">
+        <h2 className="text-5xl font-bold my-5 md:w-3/4 leading-snug">
+          Find Your Favorite <span className="text-blue-700">Book Here!</span>
+        </h2>
+        <p className="mb-10 text-lg md:w-5/6">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the standard dummy text ever since the
+          1500s, when an unknown printer took a galley of type and scrambled it
+          to make a type specimen book. It has survived not only five centuries,
+          but also the leap into electronic typesetting, remaining essentially
+          unchanged.
+        </p>
+
+        <div className="flex flex-col sm:flex-row justify-between gap-6 md:w-3/4 my-14">
+          <div>
+            <h3 className="text-3xl font-bold">800+</h3>
+            <p className="text-base">Book Listing</p>
+          </div>
+
+          <div>
+            <h3 className="text-3xl font-bold">550+</h3>
+            <p className="text-base">Register Users</p>
+          </div>
+
+          <div>
+            <h3 className="text-3xl font-bold">1200+</h3>
+            <p className="text-base">PDF Download</p>
+          </div>
+        </div>
+
+        <Link to="/shop" className="mt-12 block ">
+          <button className="bg-blue-700 text-white font-semibold px-5 py-2 rounded hover:bg-black transition-all duration-300">
+            Explore More
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default FavBook;
--- mern-client/src/Home/Home.jsx
+++ mern-client/src/Home/Home.jsx
@@ -1,11 +1,15 @@
 import Banner from "../components/Banner";
 import BestSellerBook from "./BestSellerBooks";
+import FavBook from "./FavBook";
+import PromoBanner from "./PromoBanner";
 
 const Home = () => {
   return (
     <div>
       <Banner />
       <BestSellerBook />
+      <FavBook />
+      <PromoBanner />
     </div>
   );
 };
