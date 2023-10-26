583284bdcc2d2c464c9c905eded7d8c89f50a44b
utkarsh032 <utkarshhraj525@gmail.com>
2023-10-26 22:50:44 +0530

    Book manage system is completed

--- mern-client/src/dashboard/ManageBooks.jsx
+++ mern-client/src/dashboard/ManageBooks.jsx
@@ -1,11 +1,80 @@
+import { Table } from "flowbite-react";
+import { useEffect, useState } from "react";
 
+import { FaTrash } from "react-icons/fa6";
+
+import { Link } from "react-router-dom";
 
 const ManageBooks = () => {
-  return (
-    <div>
-      Manage Books
-    </div>
-  )
-}
+  const [allBooks, setAllBooks] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:5000/all-books")
+      .then((res) => res.json())
+      .then((data) => setAllBooks(data));
+  }, []);
 
-export default ManageBooks
+  const handleDelete = (id) => {
+    fetch(`http://localhost:5000/book/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        alert("Book is deleted Successfully");
+      });
+  };
+
+  return (
+    <div className="px-4 my-12">
+      <h2 className="mb-8 text-3xl font-bold ">Managae Your Book</h2>
+
+      <Table className="lg:w-[1180px] ">
+        <Table.Head>
+          <Table.HeadCell>No.</Table.HeadCell>
+          <Table.HeadCell>Book Name</Table.HeadCell>
+          <Table.HeadCell>Author Name</Table.HeadCell>
+          <Table.HeadCell>Category</Table.HeadCell>
+          <Table.HeadCell>Price</Table.HeadCell>
+          <Table.HeadCell>
+            <span>Manage</span>
+          </Table.HeadCell>
+        </Table.Head>
+
+        {allBooks.map((book, index) => (
+          <Table.Body className="divide-y" key={book._id}>
+            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800 ">
+              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                {index + 1}
+              </Table.Cell>
+
+              <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+                {book.bookTitle}
+              </Table.Cell>
+
+              <Table.Cell>{book.authorName}</Table.Cell>
+              <Table.Cell>{book.category}</Table.Cell>
+              <Table.Cell className="items-center">$10.00</Table.Cell>
+              <Table.Cell>
+                <div className="flex flex-row items-center">
+                  <Link
+                    className="font-medium text-cyan-600 hover:underline dark:text-cyan-500 mr-5"
+                    to={`/admin/dashboard/edit-books/${book._id}`}
+                  >
+                    <p>Edit</p>
+                  </Link>
+                  <button
+                    onClick={() => handleDelete(book._id)}
+                    className="bg-red-600 p-2 font-semibold text-white rounded-md hover:bg-sky-600 "
+                  >
+                    <FaTrash />
+                  </button>
+                </div>
+              </Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        ))}
+      </Table>
+    </div>
+  );
+};
+
+export default ManageBooks;
